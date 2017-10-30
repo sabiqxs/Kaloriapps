c78f321f33a3db79149c5a19d11cfa61d45ecf4e
Sabiq Al Hasby <sabiqxs@gmail.com>
2017-10-30 23:48:13 +0700

    tambah Next Button gan

--- app/src/main/java/com/apps/awey/kaloriapps/activity_home_pages.java
+++ app/src/main/java/com/apps/awey/kaloriapps/activity_home_pages.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_home_pages extends AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class activity_home_pages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_pages);
+    }
 
+    public void nextPage(View view){
+        Intent foodIntent = new Intent(activity_home_pages.this, activity_food_pages.class);
+        startActivity(foodIntent);
+        finish();
     }
 }
